67926132aa8196abbd93a0e5bae79c2cc49ce9da
jpantana <josh.pantana@gmail.com>
2019-09-02 14:44:56 -0500

    first commit

---
+++ JungleOverloadingExercise/Classes/Employee.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JungleOverloadingExercise.Classes
+{
+    class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Employee(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        List<string> myRestaurants = new List<string>();
+
+        public void MakeRestaurantList(string restaurant)
+        {
+            myRestaurants.Add(restaurant);
+        }
+
+        public int RandomNumber(int min, int max)
+        {
+            Random random = new Random();
+            Console.WriteLine("Time for a lunch break");
+            Eat(random.Next(min, max));
+            return random.Next(min, max);
+        }
+
+        public string Eat(int index)
+        {
+            Console.WriteLine($"Looks like {FirstName} {LastName} is eating at {myRestaurants[index]}");
+            return myRestaurants[index];
+        }
+
+        public string Eat(string meal)
+        {
+            Console.WriteLine($"{FirstName} is eating {meal}");
+            return meal;
+        }
+    }
+}
---
+++ JungleOverloadingExercise/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using JungleOverloadingExercise.Classes;
+
+namespace JungleOverloadingExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your first and last name.");
+            var getFullName = Console.ReadLine();
+
+            string[] splitFirstLastName = getFullName.Split(" ");
+
+            var employee = new Employee(splitFirstLastName[0], splitFirstLastName[1]);
+
+            Console.WriteLine("What's are some restaurants in your area?");
+            var aRestaurant = Console.ReadLine();
+            String[] allRestaurants = aRestaurant.Split(", ");
+            foreach(string restaurant in allRestaurants)
+            {
+                Console.WriteLine(restaurant);
+                employee.MakeRestaurantList(restaurant);
+            };
+
+            employee.RandomNumber(1, allRestaurants.Length);
+
+            Console.WriteLine("What do you want to eat at that restaurant?");
+            var whatEmpWillEat = Console.ReadLine();
+
+            employee.Eat(whatEmpWillEat);
+        }
+    }
+}
